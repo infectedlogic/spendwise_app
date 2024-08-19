4c07d83a6e1f30b31d9be6c63ed8a94a8e39988a
infectedlogic <jeb.hazem@gmail.com>
2024-08-19 14:38:59 +0100

    commit 2

--- app.py
+++ app.py
@@ -109,6 +109,20 @@ def login():
         return 'Invalid credentials', 401
     return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            return 'Username already exists', 400
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
